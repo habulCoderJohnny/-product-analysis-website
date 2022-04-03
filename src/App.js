447d04c3386d49daeec24f5f0cfcd4bf6f127d26
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-04 02:15:06 +0600

    Created component, data of Chart

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from my React Planet!</h1>
-      </header>
+
     </div>
   );
 }
