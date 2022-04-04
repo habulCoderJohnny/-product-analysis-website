bff8ca41633c5e369e10d539994be3564919af22
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-05 00:04:20 +0600

    change title - favicn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <ul className='flex justify-center border-b-2 border-rose-600 sticky top-3 '>
+        <ul className='flex justify-center mt-4 border-b-2 border-rose-600 sticky top-0 '>
             
             <li className='-mb-px mr-1'><Link to="/" className='bg-blue-600 inline-block text-lime-300  border-l border-t border-r rounded-t py-2 px-4 hover:text-white font-semibold'>HOME</Link></li>
 
